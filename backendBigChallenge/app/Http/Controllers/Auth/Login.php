76aaa82d8d81069c7aec2df6637d6e8fe51879f7
Francisco Rossi <francisco.rossi@lightit.io>
2023-05-10 15:30:49 -0300

    Refactor to use formrequest and SingleAction controller

---
+++ backendBigChallenge/app/Http/Controllers/Auth/Login.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginPostRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class Login extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(LoginPostRequest $request)
+    {
+        $request->validated();
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+        $token = $user->createToken($request->device_name)->plainTextToken;
+        return response()->json(['token' => $token, 'user' => $user->only(['id', 'name', 'email'])]);
+    }
+}
