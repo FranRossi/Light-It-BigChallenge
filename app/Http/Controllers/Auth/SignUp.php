27134b0ebba7de05e817c173927a1b09c7d7357d
Francisco Rossi <francisco.rossi@lightit.io>
2023-05-11 12:35:06 -0300

    fix code errors to stop using hash facades

--- app/Http/Controllers/Auth/SignUp.php
+++ app/Http/Controllers/Auth/SignUp.php
@@ -7,16 +7,16 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SignUpRequest;
 use App\Models\User;
-use Hash;
+use \Illuminate\Hashing\HashManager;
 use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\JsonResponse;
 
 class SignUp extends Controller
 {
-    public function __invoke(SignUpRequest $request): JsonResponse|ResponseFactory
+    public function __invoke(SignUpRequest $request, HashManager $hash): JsonResponse|ResponseFactory
     {
         $fields = $request->validated();
-        $fields['password'] = Hash::make($fields['password']);
+        $fields['password'] = $hash->make($fields['password']);
         $user = User::create($fields);
 
         $user->assignRole($request['role']);
